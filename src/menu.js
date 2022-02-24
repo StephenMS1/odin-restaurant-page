266b31e9dab10caba3b9c64276c62cbc780f48dd
Stephen MS <stephen.mcilroy.sharpe@gmail.com>
2022-02-24 11:36:30 +0000

    completion of menu page

--- src/menu.js
+++ src/menu.js
@@ -1,10 +1,16 @@
-import hotDog from './hot dog.png'
+import hotDog from './hot dog.png';
+import pupCakes from './pupcakes.jpg';
+import turtleLoaf from './turtleLoaf.jpg';
+import dolphinFruitCup from './dolphinFruitCup.jpg';
 
 function menuPage(){
     let menuScreen = document.createElement('div');
     menuScreen.classList.add('menuPage');
 
-    menuScreen.appendChild(createMenuItem('Hot Dog', 'Pupper bun holding a sausage!'));
+    menuScreen.appendChild(createMenuItem('Hot Dog', 'Pupper bun holding a sausage!', hotDog));
+    menuScreen.appendChild(createMenuItem('Turtle Soup Loaf', 'Turtle shaped loaf full of delicious caramelised french onion soup', turtleLoaf));
+    menuScreen.appendChild(createMenuItem('Pupcakes', 'Pupper shaped pancakes with nutella and fruit!', pupCakes));
+    menuScreen.appendChild(createMenuItem('Dolphin Fruit Cup', 'Mixture of the freshest forest fruits', dolphinFruitCup));
 
     return menuScreen;
 }
@@ -14,7 +20,7 @@ function loadMenuPage(pageSpace){
     pageSpace.append(menuPage());
 }
 
-function createMenuItem(name, description){
+function createMenuItem(name, description, imgSrc){
     let itemCard = document.createElement('div');
     itemCard.classList.add('itemCard');
 
@@ -25,7 +31,7 @@ function createMenuItem(name, description){
     itemDescription.textContent = `${description}`;
 
     let itemImage = document.createElement('img');
-    itemImage.src = hotDog;
+    itemImage.src = imgSrc;
 
     itemCard.append(itemImage, itemName, itemDescription)
 
